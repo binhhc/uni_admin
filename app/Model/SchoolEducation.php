b670f07954c821fe89746370dfa1050c223eaa40
binhhc <binhhc@leverages.jp>
2013-12-20 11:41:53 +0700

    fix validate model

--- app/Model/SchoolEducation.php
+++ app/Model/SchoolEducation.php
@@ -13,11 +13,7 @@ class SchoolEducation extends AppModel {
                     'rule' => 'notEmpty',
                     'allowEmpty' => false,
                     'message' => 'Employee id not empty!'
-                ),
-                'numeric' => array(
-                    'rule' => 'numeric',
-                    'message' => 'Numbers only'
-                ),
+                ),                
             ),
             'graduate_type_cd' => array(
                 'numeric' => array(
